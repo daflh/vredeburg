2b01ccb66ce7ca7e20b6db8ac4ed3d3939012ee2
Dafiul Haq <dafiulhaq97@gmail.com>
2020-08-03 13:32:12 +0700

    update site data

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
     title: "Vredeburg",
-    description: "Put your description here",
+    description: "Eleventy starter project to create a blog",
     keywords: ["eleventy", "template", "simple", "clean"],
-    url: "https://vredeburg.netlify.app", // deploy website url without trailing slash
-    paginate: 6, // how many posts are displayed on one page
-    // disqusShortname: "your-shortname"
+    url: "https://vredeburg.netlify.app", // don't include trailing slash!
+    paginate: 6, // how many posts do you want for each page
+    // disqusShortname: "your-shortname" // only if you want to add disqus to your site
 }
